05e6a83b92fe168a8d95b3948417fa9aa1956656
Maxim Zhiburt <zhiburt@gmail.com>
2019-05-06 16:59:22 +0300

    Update packages

--- pie/float64s_pie.go
+++ pie/float64s_pie.go
@@ -350,6 +350,46 @@ func (ss Float64s) Send(ctx context.Context, ch chan<- float64) Float64s {
 	return ss
 }
 
+// Sequence returns a sequencent elements.
+//
+// There are 3 variations to generate:
+// 		1. [0, n).
+//		2. [min, max).
+//		3. [min, max) with step.
+//
+// if lenght of params == 1 considered that will returned slice between 0 and n,
+// where n is the first param, [0, n).
+// if lenght of params == 2 considered that will returned slice between min and max,
+// where min is the first param, max is the second, [min, max).
+// if lenght of params == 3 considered that will returned slice between min and max with step,
+// where min is the first param, max is the second, step is the third one, [min, max) with step.
+//
+// if len(params) > 3, uses 3 approach, with min, max and step others params will be ignored
+func (ss Float64s) Sequence(params ...int) Float64s {
+	var seq = func(min, max, step int) (seq Float64s) {
+		lenght := int(math.Round(float64(max-min) / float64(step)))
+		if lenght < 1 {
+			return
+		}
+
+		seq = make(Float64s, lenght)
+		for i := 0; i < lenght; min += step {
+			seq[i] = float64(min)
+			i++
+		}
+
+		return seq
+	}
+
+	if len(params) > 2 {
+		return seq(params[0], params[1], params[2])
+	} else if len(params) > 1 {
+		return seq(params[0], params[1], 1)
+	} else {
+		return seq(0, params[0], 1)
+	}
+}
+
 // Sort works similar to sort.Float64s(). However, unlike sort.Float64s the
 // slice returned will be reallocated as to not modify the input slice.
 //
--- pie/ints_pie.go
+++ pie/ints_pie.go
@@ -350,6 +350,46 @@ func (ss Ints) Send(ctx context.Context, ch chan<- int) Ints {
 	return ss
 }
 
+// Sequence returns a sequencent elements.
+//
+// There are 3 variations to generate:
+// 		1. [0, n).
+//		2. [min, max).
+//		3. [min, max) with step.
+//
+// if lenght of params == 1 considered that will returned slice between 0 and n,
+// where n is the first param, [0, n).
+// if lenght of params == 2 considered that will returned slice between min and max,
+// where min is the first param, max is the second, [min, max).
+// if lenght of params == 3 considered that will returned slice between min and max with step,
+// where min is the first param, max is the second, step is the third one, [min, max) with step.
+//
+// if len(params) > 3, uses 3 approach, with min, max and step others params will be ignored
+func (ss Ints) Sequence(params ...int) Ints {
+	var seq = func(min, max, step int) (seq Ints) {
+		lenght := int(math.Round(float64(max-min) / float64(step)))
+		if lenght < 1 {
+			return
+		}
+
+		seq = make(Ints, lenght)
+		for i := 0; i < lenght; min += step {
+			seq[i] = int(min)
+			i++
+		}
+
+		return seq
+	}
+
+	if len(params) > 2 {
+		return seq(params[0], params[1], params[2])
+	} else if len(params) > 1 {
+		return seq(params[0], params[1], 1)
+	} else {
+		return seq(0, params[0], 1)
+	}
+}
+
 // Sort works similar to sort.Ints(). However, unlike sort.Ints the
 // slice returned will be reallocated as to not modify the input slice.
 //
