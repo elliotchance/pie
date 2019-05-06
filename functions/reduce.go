1675a5639575e3a376cf839fc0284a6bf7bc3c28
dylan <meeusdylan@protonmail.com>
2019-05-07 01:41:07 +0200

    Update documentation for reduce.go

--- functions/reduce.go
+++ functions/reduce.go
@@ -3,7 +3,7 @@ package functions
 // Reduce continually applies the provided function
 // over the slice. Reducing the elements to a single value.
 //
-// returns: zero value of type if no elements in slice or no function provided.
+// Returns a zero value of ElementType if there are no elements in the slice. It will panic if the reducer is nil and the slice has more than one element (required to invoke reduce).
 // Otherwise returns result of applying reducer from left to right.
 func (ss SliceType) Reduce(reducer func(ElementType, ElementType) ElementType) (el ElementType) {
 	if len(ss) == 0 {
