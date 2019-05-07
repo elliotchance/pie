b144792180caf39693edc5100cf258c578da2e8a
Maxim Zhiburt <zhiburt@gmail.com>
2019-05-07 12:01:59 +0300

    Change comment on sequence function

--- functions/sequence.go
+++ functions/sequence.go
@@ -2,21 +2,20 @@ package functions
 
 import "math"
 
-// Sequence returns a sequencent elements.
+// Sequence generates all numbers in range or returns nil if params invalid
 //
 // There are 3 variations to generate:
 // 		1. [0, n).
 //		2. [min, max).
 //		3. [min, max) with step.
 //
-// if lenght of params == 1 considered that will returned slice between 0 and n,
+// if len(params) == 1 considered that will be returned slice between 0 and n,
 // where n is the first param, [0, n).
-// if lenght of params == 2 considered that will returned slice between min and max,
+// if len(params) == 2 considered that will be returned slice between min and max,
 // where min is the first param, max is the second, [min, max).
-// if lenght of params == 3 considered that will returned slice between min and max with step,
-// where min is the first param, max is the second, step is the third one, [min, max) with step.
-//
-// if len(params) > 3, uses 3 approach, with min, max and step others params will be ignored
+// if len(params) > 2 considered that will be returned slice between min and max with step,
+// where min is the first param, max is the second, step is the third one, [min, max) with step,
+// others params will be ignored
 func (ss SliceType) Sequence(params ...int) SliceType {
 	var seq = func(min, max, step int) (seq SliceType) {
 		lenght := int(math.Round(float64(max-min) / float64(step)))
--- pie/float64s_pie.go
+++ pie/float64s_pie.go
@@ -350,21 +350,20 @@ func (ss Float64s) Send(ctx context.Context, ch chan<- float64) Float64s {
 	return ss
 }
 
-// Sequence returns a sequencent elements.
+// Sequence generates all numbers in range or returns nil if params invalid
 //
 // There are 3 variations to generate:
 // 		1. [0, n).
 //		2. [min, max).
 //		3. [min, max) with step.
 //
-// if lenght of params == 1 considered that will returned slice between 0 and n,
+// if len(params) == 1 considered that will be returned slice between 0 and n,
 // where n is the first param, [0, n).
-// if lenght of params == 2 considered that will returned slice between min and max,
+// if len(params) == 2 considered that will be returned slice between min and max,
 // where min is the first param, max is the second, [min, max).
-// if lenght of params == 3 considered that will returned slice between min and max with step,
-// where min is the first param, max is the second, step is the third one, [min, max) with step.
-//
-// if len(params) > 3, uses 3 approach, with min, max and step others params will be ignored
+// if len(params) > 2 considered that will be returned slice between min and max with step,
+// where min is the first param, max is the second, step is the third one, [min, max) with step,
+// others params will be ignored
 func (ss Float64s) Sequence(params ...int) Float64s {
 	var seq = func(min, max, step int) (seq Float64s) {
 		lenght := int(math.Round(float64(max-min) / float64(step)))
