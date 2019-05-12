8a3402989b93abae8863bc48c6b71f9188e3a4a2
Deleplace <deleplace2015@gmail.com>
2019-05-12 18:18:27 +0200

    Join more efficient impl: only 1 allocation.

--- pie/strings_pie.go
+++ pie/strings_pie.go
@@ -285,15 +285,33 @@ func (ss Strings) Ints() Ints {
 
 // Join returns a string from joining each of the elements.
 func (ss Strings) Join(glue string) (s string) {
+	// Ideally we would call directly strings.Join, however the code generation
+	// makes it non-trivial to have the correct parameter type everywhere.
+
+	// We use the same idea, which is to make only 1 string allocation for the result.
+	if len(ss) == 0 {
+		return ""
+	}
+	targetLength := 0
+	for _, element := range ss {
+		targetLength += len(element)
+	}
+	nglue := len(glue)
+	targetLength += (len(ss) - 1) * nglue
+
+	buffer := make([]byte, targetLength)
+	b := buffer[:]
 	for i, element := range ss {
 		if i > 0 {
-			s += glue
+			copy(b[:nglue], glue)
+			b = b[nglue:]
 		}
-
-		s += string(element)
+		n := len(element)
+		copy(b[:n], element)
+		b = b[n:]
 	}
 
-	return s
+	return string(buffer)
 }
 
 // JSONString returns the JSON encoded array as a string.
