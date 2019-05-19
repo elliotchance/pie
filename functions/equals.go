dd4aa2213b382481c4efd0be4a527f2d90661b93
Maxim Zhiburt <zhiburt@gmail.com>
2019-05-19 15:15:25 +0300

    Change comment on equals function

--- functions/equals.go
+++ functions/equals.go
@@ -3,6 +3,8 @@ package functions
 // Equals compare elements of slice
 //
 // if all elements the same is considered that slices are equal
+// if len(first_slice) != len(second_slice) they are not equal
+// if slices == nil is considered that they're equal
 //
 // if element realizes Equals interface it uses that method, in other way uses default compare
 func (ss SliceType) Equals(rhs SliceType) bool {
