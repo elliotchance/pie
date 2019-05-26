b8aaf08eccf7d8c20ac6ae632c8fa8bffda81ffa
ougwen1235 <ougwen1235@gmail.com>
2019-05-27 07:18:26 +0800

    ElementZeroValue should be nil (#153)
    ElementZeroValue should be nil when dealing with structs, instead of a new allocation.
    
    Fixes #152

--- main.go
+++ main.go
@@ -210,7 +210,7 @@ func main() {
 			// If its a pointer we need to replace '*' -> '&' when
 			// instantiating.
 			if elementType[0] == '*' {
-				zeroValue = "&" + zeroValue[1:]
+				zeroValue = "nil"
 			}
 
 			t = strings.Replace(t, "ElementZeroValue", zeroValue, -1)
