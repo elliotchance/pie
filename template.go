da9b2cec785a9bebeba3c9cda88940135707ca2b
Luoxin <luoxin.ttt@gmail.com>
2022-06-23 19:04:51 +0800

    chunk

--- template.go
+++ template.go
@@ -1260,4 +1260,27 @@ func (m MapType) Values() []ElementType {
 	return keys
 }
 `,
+	
+	"Chunk": `package functions
+
+import "fmt"
+
+// Chunk Split slice to chunks
+func (ss SliceType) Chunk(chunkSize int, callback func(chunk SliceType) (stopped bool)) {
+	if chunkSize <= 0 {
+		panic(fmt.Sprintf("invalid chunk size %d", chunkSize))
+	}
+	var stopped bool
+	for i := 0; i < len(ss); i += chunkSize {
+		if i+chunkSize < len(ss) {
+			stopped = callback(ss[i : i+chunkSize])
+		} else {
+			stopped = callback(ss[i:])
+		}
+		if stopped {
+			break
+		}
+	}
+}
+`,
 }
