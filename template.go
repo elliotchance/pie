6a0be60d5d72fecc5b06fecd4213be983baacfb7
Luoxin <luoxin.ttt@gmail.com>
2022-06-23 19:06:04 +0800

    generate

--- template.go
+++ template.go
@@ -116,6 +116,28 @@ func (ss SliceType) Bottom(n int) (top SliceType) {
 	return
 }
 `,
+	"Chunk": `package functions
+
+import "fmt"
+
+// Chunk Split slice to chunks
+func (ss SliceType) Chunk(chunkSize int, callback func(chunk SliceType) (stopped bool)) {
+	if chunkSize <= 0 {
+		panic(fmt.Sprintf("invalid chunk size %d", chunkSize))
+	}
+	var stopped bool
+	for i := 0; i < len(ss); i += chunkSize {
+		if i+chunkSize < len(ss) {
+			stopped = callback(ss[i : i+chunkSize])
+		} else {
+			stopped = callback(ss[i:])
+		}
+		if stopped {
+			break
+		}
+	}
+}
+`,
 	"Contains": `package functions
 
 // Contains returns true if the element exists in the slice.
@@ -1258,29 +1280,6 @@ func (m MapType) Values() []ElementType {
 	}
 
 	return keys
-}
-`,
-	
-	"Chunk": `package functions
-
-import "fmt"
-
-// Chunk Split slice to chunks
-func (ss SliceType) Chunk(chunkSize int, callback func(chunk SliceType) (stopped bool)) {
-	if chunkSize <= 0 {
-		panic(fmt.Sprintf("invalid chunk size %d", chunkSize))
-	}
-	var stopped bool
-	for i := 0; i < len(ss); i += chunkSize {
-		if i+chunkSize < len(ss) {
-			stopped = callback(ss[i : i+chunkSize])
-		} else {
-			stopped = callback(ss[i:])
-		}
-		if stopped {
-			break
-		}
-	}
 }
 `,
 }
